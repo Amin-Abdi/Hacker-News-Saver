0a4b38344eda28d4aa9eac7d6edb51cbd0dd2ff8
Amin <malmaaz78@gmail.com>
2021-09-12 11:56:46 +0100

    Implemented fetch using useContext

--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import SinglePost from "./SinglePost";
 import Loading from "./Loading";
+import { useGlobalContext } from "../Context";
 
 const PostList = () => {
-  const [loading, setLoading] = useState(false);
+  const { posts, loading, searchTerm, setSearchTerm } = useGlobalContext();
+
+  console.log("PostList:", posts);
 
   if (loading) {
     return <Loading />;
+  }
+
+  if (posts.length < 1) {
+    return (
+      <div className="no-list">
+        <h2>Could not find posts that match your criteria</h2>
+      </div>
+    );
   }
 
   return (
--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,44 @@
 import React, { useState, useContext, useEffect } from "react";
 
 const AppContext = React.createContext();
+const URL = "https://hn.algolia.com/api/v1/search?";
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("java");
+
+  const fetchPosts = async () => {
+    // setLoading(true)
+    try {
+      const response = await fetch(`${URL}query=${searchTerm}`);
+      const postData = await response.json();
+      //console.log(postData.hits);
+      const items = postData.hits;
+      //console.log(items);
+
+      //Checkign if they are empty
+      if (items) {
+        const newPost = items.map((single) => {
+          const { author, num_comments, objectID, points, title, url } = single;
+          return { author, num_comments, objectID, points, title, url };
+        });
+        setPosts(newPost);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ loading, posts, searchTerm, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
